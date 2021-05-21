8089cee3c2506d21b8129dfc7f213539e491fa8b
Francesco <f.tabani@libero.it>
2021-05-21 23:04:34 +0200

    updates for Heroku deplyment

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t^qf&okfn_-#()a5bho33t-tfxp(6lx1_5111-&4gml1$6@c$n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
